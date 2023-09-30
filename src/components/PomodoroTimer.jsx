4798d6f730c12c8a70fbd1a1d1b263a7488da024
seymoreg <seymur.gul1y3v@gmail.com>
2023-10-01 01:35:11 +0400

    "reset and configuration buttons added"

--- src/components/PomodoroTimer.jsx
+++ src/components/PomodoroTimer.jsx
@@ -1,8 +1,11 @@
 import React, { useState, useEffect } from "react";
 
 const PomodoroTimer = () => {
+  const initialTimer = 1500;
   const [timer, setTimer] = useState(1500);
   const [isActive, setIsActive] = useState(false);
+  const [showConfigModal, setShowConfigModal] = useState(false);
+  const [newTimerValue, setNewTimerValue] = useState(initialTimer);
 
   useEffect(() => {
     let interval;
@@ -26,6 +29,11 @@ const PomodoroTimer = () => {
     setIsActive(false);
   };
 
+  const resetTimer = () => {
+    setIsActive(false);
+    setTimer(initialTimer);
+  };
+
   const formatTime = (seconds) => {
     const minutes = Math.floor(seconds / 60)
       .toString()
@@ -34,20 +42,89 @@ const PomodoroTimer = () => {
     return `${minutes} : ${remainingSeconds}`;
   };
 
+  const openConfigModal = () => {
+    setShowConfigModal(true);
+  };
+
+  const closeConfigModal = () => {
+    setShowConfigModal(false);
+  };
+
+  const handleTimerInputChange = (event) => {
+    const inputValue = event.target.value;
+    setNewTimerValue(inputValue);
+  };
+
+  const updateTimer = () => {
+    setTimer(newTimerValue);
+    closeConfigModal();
+  };
+
   return (
-    <div className="flex flex-col justify-center items-center flex-grow bg-secondary px-96">
+    <div className="flex flex-col justify-center items-center flex-grow bg-secondary px-96 rounded-md">
       {/* screen where timer will be shown */}
-      <div className="text-7xl">{formatTime(timer)}</div>
+      <div className="text-tertiary text-7xl">{formatTime(timer)}</div>
       {/* buttons of control start/stop/short_break/long_break */}
-      <div>
+      <div className="py-4 text-lg">
         {!isActive ? (
-          <button onClick={startTimer}>Start</button>
+          <button
+            className="text-tertiary border p-1 px-5 mx-5 rounded-md hover:text-primary hover:bg-tertiary"
+            onClick={startTimer}
+          >
+            START
+          </button>
         ) : (
-          <button onClick={stopTimer}>Stop</button>
+          <button
+            className="text-tertiary border p-1 px-5 mx-5 rounded-md hover:text-primary hover:bg-tertiary"
+            onClick={stopTimer}
+          >
+            STOP
+          </button>
         )}
+        <button
+          className="text-tertiary border p-1 px-5 mx-5 rounded-md hover:text-primary hover:bg-tertiary"
+          onClick={resetTimer}
+        >
+          RESET
+        </button>
       </div>
       {/* settings button where you may customize your timer */}
-      <div>settings button</div>
+      <div>
+        <button
+          className="text-tertiary border p-1 px-5 mx-5 rounded-md hover:text-primary hover:bg-tertiary"
+          onClick={openConfigModal}
+        >
+          CONFIGURATION
+        </button>
+        {showConfigModal && (
+          <div className="absolute top-0 left-0 w-full h-full flex items-center justify-center bg-opacity-50 bg-gray-800">
+            <div className="bg-white p-4 rounded-lg">
+              <h2 className="text-tertiary text-2xl mb-4">
+                Timer Configuration
+              </h2>
+              <label className="block mb-2">Enter Timer Value (seconds):</label>
+              <input
+                type="number"
+                value={newTimerValue}
+                onChange={handleTimerInputChange}
+                className="border p-2 rounded-md w-full"
+              />
+              <button
+                className="mt-4 text-tertiary border p-2 px-4 rounded-md bg-primary hover:bg-tertiary hover:text-primary"
+                onClick={updateTimer}
+              >
+                Update Timer
+              </button>
+              <button
+                className="mt-4 text-tertiary border p-2 px-4 rounded-md bg-primary hover:bg-tertiary hover:text-primary"
+                onClick={closeConfigModal}
+              >
+                Cancel
+              </button>
+            </div>
+          </div>
+        )}
+      </div>
       {/* todo list task where you may add/delete/modify and put a checkmark on task */}
       <div>todo list</div>
     </div>
