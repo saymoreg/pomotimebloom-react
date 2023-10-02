d05d72c7820b8e065653f830730110f07200b05b
seymoreg <seymur.gul1y3v@gmail.com>
2023-10-03 00:11:19 +0400

    short, long breaks and its configurations added

--- src/components/PomodoroTimer.jsx
+++ src/components/PomodoroTimer.jsx
@@ -1,11 +1,23 @@
 import React, { useState, useEffect } from "react";
 
 const PomodoroTimer = () => {
-  const initialTimer = 1500;
-  const [timer, setTimer] = useState(1500);
+  const initialTimerMinutes = 25;
+  const initialShortBreakMinutes = 5;
+  const initialLongBreakMinutes = 15;
+
+  const [timer, setTimer] = useState(initialTimerMinutes * 60);
   const [isActive, setIsActive] = useState(false);
   const [showConfigModal, setShowConfigModal] = useState(false);
-  const [newTimerValue, setNewTimerValue] = useState(initialTimer);
+  const [newTimerValue, setNewTimerValue] = useState(initialTimerMinutes * 60);
+  const [newShortBreakValue, setNewShortBreakValue] = useState(
+    initialShortBreakMinutes * 60
+  );
+  const [newLongBreakValue, setNewLongBreakValue] = useState(
+    initialLongBreakMinutes * 60
+  );
+
+  const [isShortBreak, setIsShortBreak] = useState(false);
+  const [isLongBreak, setIsLongBreak] = useState(false);
 
   useEffect(() => {
     let interval;
@@ -18,11 +30,28 @@ const PomodoroTimer = () => {
       setIsActive(false);
     }
 
+    if (isShortBreak) {
+      setTimer(newShortBreakValue);
+      setIsShortBreak(false);
+    } else if (isLongBreak) {
+      setTimer(newLongBreakValue);
+      setIsLongBreak(false);
+    }
+
     return () => clearInterval(interval);
-  }, [isActive, timer]);
+  }, [
+    isActive,
+    timer,
+    isShortBreak,
+    isLongBreak,
+    newShortBreakValue,
+    newLongBreakValue,
+  ]);
 
   const startTimer = () => {
     setIsActive(true);
+    setIsShortBreak(false);
+    setIsLongBreak(false);
   };
 
   const stopTimer = () => {
@@ -31,15 +60,17 @@ const PomodoroTimer = () => {
 
   const resetTimer = () => {
     setIsActive(false);
-    setTimer(initialTimer);
+    setTimer(initialTimerMinutes * 60);
+    setIsShortBreak(false);
+    setIsLongBreak(false);
   };
 
   const formatTime = (seconds) => {
-    const minutes = Math.floor(seconds / 60)
-      .toString()
-      .padStart(2, "0");
-    const remainingSeconds = (seconds % 60).toString().padStart(2, "0");
-    return `${minutes} : ${remainingSeconds}`;
+    const minutes = Math.floor(seconds / 60);
+    const remainingSeconds = seconds % 60;
+    const formattedMinutes = minutes.toString().padStart(2, "0");
+    const formattedSeconds = remainingSeconds.toString().padStart(2, "0");
+    return `${formattedMinutes}:${formattedSeconds}`;
   };
 
   const openConfigModal = () => {
@@ -51,8 +82,18 @@ const PomodoroTimer = () => {
   };
 
   const handleTimerInputChange = (event) => {
-    const inputValue = event.target.value;
-    setNewTimerValue(inputValue);
+    const inputValue = event.target.valueAsNumber;
+    setNewTimerValue(inputValue * 60);
+  };
+
+  const handleShortBreakInputChange = (event) => {
+    const inputValue = event.target.valueAsNumber;
+    setNewShortBreakValue(inputValue * 60);
+  };
+
+  const handleLongBreakInputChange = (event) => {
+    const inputValue = event.target.valueAsNumber;
+    setNewLongBreakValue(inputValue * 60);
   };
 
   const updateTimer = () => {
@@ -60,19 +101,53 @@ const PomodoroTimer = () => {
     closeConfigModal();
   };
 
+  const startShortBreak = () => {
+    setIsShortBreak(true);
+    setIsLongBreak(false);
+    setTimer(newShortBreakValue);
+    setIsActive(true);
+  };
+
+  const startLongBreak = () => {
+    setIsLongBreak(true);
+    setIsShortBreak(false);
+    setTimer(newLongBreakValue);
+    setIsActive(true);
+  };
+
   return (
     <div className="flex flex-col justify-center items-center flex-grow bg-secondary px-96 rounded-md">
       {/* screen where timer will be shown */}
-      <div className="text-tertiary text-7xl">{formatTime(timer)}</div>
+      <div className="text-tertiary text-7xl">
+        {isShortBreak
+          ? formatTime(newShortBreakValue)
+          : isLongBreak
+          ? formatTime(newLongBreakValue)
+          : formatTime(timer)}
+      </div>
       {/* buttons of control start/stop/short_break/long_break */}
       <div className="py-4 text-lg">
         {!isActive ? (
-          <button
-            className="text-tertiary border p-1 px-5 mx-5 rounded-md hover:text-primary hover:bg-tertiary"
-            onClick={startTimer}
-          >
-            START
-          </button>
+          <>
+            <button
+              className="text-tertiary border p-1 px-5 mx-5 rounded-md hover:text-primary hover:bg-tertiary"
+              onClick={startTimer}
+            >
+              START
+            </button>
+            <button
+              className="text-tertiary border p-1 px-5 mx-5 rounded-md hover:text-primary hover:bg-tertiary"
+              onClick={startShortBreak}
+            >
+              SHORT BREAK
+            </button>
+            <button
+              className="text-tertiary border p-1 px-5 mx-5 rounded-md hover:text-primary hover:bg-tertiary"
+              onClick={startLongBreak}
+            >
+              LONG BREAK
+            </button>
+          </>
         ) : (
           <button
             className="text-tertiary border p-1 px-5 mx-5 rounded-md hover:text-primary hover:bg-tertiary"
@@ -99,14 +174,34 @@ const PomodoroTimer = () => {
         {showConfigModal && (
           <div className="absolute top-0 left-0 w-full h-full flex items-center justify-center bg-opacity-50 bg-gray-800">
             <div className="bg-white p-4 rounded-lg">
-              <h2 className="text-tertiary text-2xl mb-4">
+              <h2 className="text-primary text-2xl mb-4">
                 Timer Configuration
               </h2>
-              <label className="block mb-2">Enter Timer Value (seconds):</label>
+              <label className="block mb-2 text-primary">
+                Enter Timer Value (minutes):
+              </label>
               <input
                 type="number"
-                value={newTimerValue}
+                value={newTimerValue / 60}
                 onChange={handleTimerInputChange}
+                className="border p-2 rounded-md w-full"
+              />
+              <label className="block mb-2 text-primary">
+                Short Break Timer Value (minutes):
+              </label>
+              <input
+                type="number"
+                value={newShortBreakValue / 60}
+                onChange={handleShortBreakInputChange}
+                className="border p-2 rounded-md w-full"
+              />
+              <label className="block mb-2 text-primary">
+                Long Break Timer Value (minutes):
+              </label>
+              <input
+                type="number"
+                value={newLongBreakValue / 60}
+                onChange={handleLongBreakInputChange}
                 className="border p-2 rounded-md w-full"
               />
               <button
